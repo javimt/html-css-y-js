08eb19214e86bffe06802ba1192dbc8e38fb09d2
javimt <javiergp_19@outlook.com>
2023-06-05 22:24:31 -0500

    some changes on carrusel n gallery image

--- carrusel/index.js
+++ carrusel/index.js
@@ -1,12 +1,19 @@
-function openPreview(image) {
-    const previewContainer = document.querySelector('.preview-container');
-    const previewImage = document.querySelector('.preview-image');
-  
-    previewImage.src = image.src;
-    previewContainer.style.display = 'flex';
-  }
-  
-  function closePreview() {
-    const previewContainer = document.querySelector('.preview-container');
-    previewContainer.style.display = 'none';
-  }
+function openPreview(event, image) {
+
+  event.stopPropagation();
+
+  const previewContainer = document.querySelector(".preview-container");
+  const previewImage = document.querySelector(".preview-image");
+
+  previewImage.src = image;
+  previewContainer.style.display = "flex";
+}
+
+function closePreview() {
+  const previewContainer = document.querySelector(".preview-container");
+  previewContainer.style.display = "none";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  closePreview();
+});
